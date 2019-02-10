d8ce4979ea49d047aaa0d684c3cb21480e2c3224
Mateusz Woźniczka <mwozniczka@transper.com>
2019-02-10 16:19:41 +0100

    benchmark added

--- pe_001.rb
+++ pe_001.rb
@@ -1,3 +1,12 @@
+require 'benchmark'
 require_relative 'pe_001_brute_force'
 
-print PE001BruteForce.sum_of_multiples(1000)
+limit = 1000
+n=5000
+Benchmark.bm do |benchmark|
+  benchmark.report('brute_force') do
+    n.times do
+      PE001BruteForce.sum_of_multiples(limit)
+    end
+  end
+end
