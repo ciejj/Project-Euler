df353f351017529f51a1567c2d1c3d308f487e0f
Mateusz Woźniczka <mwozniczka@transper.com>
2019-02-10 16:44:25 +0100

    geometric soltion implemented

--- pe_001.rb
+++ pe_001.rb
@@ -2,16 +2,21 @@ require 'benchmark'
 require_relative 'sum_of_multiples'
 
 limit = 1000
-n=500
+n=5000
 
-puts "Brute Force result: #{SumOfMultiples.brute_force(limit)}" 
-
-puts "----"
+puts "Brute Force result: #{SumOfMultiples.brute_force(limit)}"
+puts "geometric result: #{SumOfMultiples.geometric(limit)}"
+puts '----'
 
 Benchmark.bm do |benchmark|
   benchmark.report('brute_force') do
     n.times do
       SumOfMultiples.brute_force(limit)
+    end
+  end
+  benchmark.report('geometric') do
+    n.times do
+      SumOfMultiples.geometric(limit)
     end
   end
 end
--- sum_of_multiples.rb
+++ sum_of_multiples.rb
@@ -1,5 +1,13 @@
 class SumOfMultiples
   def self.brute_force(limit)
-    (1...limit).inject { |sum, i| (i % 3).zero? || (i % 5).zero? ? sum + i : sum }
+    (0...limit).inject { |sum, i| (i % 3).zero? || (i % 5).zero? ? sum + i : sum }
+  end
+
+  def self.geometric(limit)
+    sum_of_x_multiples(3, limit-1) + sum_of_x_multiples(5, limit-1) - sum_of_x_multiples(15, limit-1)
+  end
+
+  def self.sum_of_x_multiples(x, limit)
+    x * (limit / x) * ((limit / x) + 1) / 2
   end
 end
