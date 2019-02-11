e2b101c1b448d0111900be98b4779f78e169b9b9
ciejj <wozniczka.mateusz@gmail.com>
2019-02-11 18:31:44 +0100

    minor code corrections

--- PE003/pe_003.rb
+++ PE003/pe_003.rb
@@ -1,24 +1,16 @@
 require 'benchmark'
 require_relative 'prime_factors'
 
-limit = 13195
+big_number = 7
 n=5000
 
-puts "Brute Force result: #{PrimeFactors.brute_force(limit)}"
+puts "Brute Force result: #{PrimeFactors.brute_force(big_number)}"
 puts '----'
 
-=begin
 Benchmark.bm do |benchmark|
   benchmark.report('brute_force') do
     n.times do
-        EvenFibonacciNumbers.brute_force(limit)
+      PrimeFactors.brute_force(big_number)
     end
   end
-  benchmark.report('every_third') do
-    n.times do
-        EvenFibonacciNumbers.every_third(limit)
-    end
-  end
-
 end
-=end
--- PE003/prime_factors.rb
+++ PE003/prime_factors.rb
@@ -1,21 +1,26 @@
 require 'pry'
 
 class PrimeFactors
-
-  def self.brute_force(limit)
+  def self.brute_force(big_number)
     i = 2
-    factors_array = Array.new
+    factors_array = []
 
-    while i <= limit
-
-      if (limit % i).zero?
-        #binding.pry
+    # list all factors of the big_number
+    while i <= big_number
+      if (big_number % i).zero?
         factors_array << i
-        limit /= i
+        big_number /= i
       else i += 1
       end
     end
 
-    factors_array.last
+    # find the biggest prime factor
+    factors_array.reverse_each { |f| return f if prime?(f) }
+  end
+
+  # returns true, if the number is prime
+  def self.prime?(number)
+    2.upto(number - 1).each { |k| return false if (number % k).zero? }
+    true
   end
 end
