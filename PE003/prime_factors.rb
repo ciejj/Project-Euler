b6370cf40c14a822401a805089a7640490624994
ciejj <wozniczka.mateusz@gmail.com>
2019-02-11 18:02:15 +0100

    code reworked

--- PE003/prime_factors.rb
+++ PE003/prime_factors.rb
@@ -3,19 +3,19 @@ require 'pry'
 class PrimeFactors
 
   def self.brute_force(limit)
-    i=2
+    i = 2
     factors_array = Array.new
 
-    while (i <= limit)
+    while i <= limit
 
-      if limit % i == 0
+      if (limit % i).zero?
         #binding.pry
-      factors_array << i
-      limit = limit/i
-      else i+=1
+        factors_array << i
+        limit /= i
+      else i += 1
       end
     end
-    
+
     factors_array.last
   end
 end
