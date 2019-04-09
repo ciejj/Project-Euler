8d4ba1d8f86f87f2065c6408c7417c991330ffee
ciejj <wozniczka.mateusz@gmail.com>
2019-04-09 20:14:58 +0200

    PE007 code corrections

--- PE007/prime_finder.rb
+++ PE007/prime_finder.rb
@@ -1,35 +1,32 @@
-module PrimeFinder 
-
-    def self.is_prime?(number)
-        if number <= 1
-            return false
-        elsif number == 2
-            return true
-        elsif number %2==0
-            return false
-        end
-
-        i=3
-        while i*i <= number
-          if (number % i).zero? 
-            return false
-          else 
-            i+=2
-          end
-        end
-        true
-
+module PrimeFinder
+  def self.prime?(number)
+    if number <= 1
+      return false
+    elsif number == 2
+      return true
+    elsif number.even?
+      return false
     end
 
-    def self.brute_force(number_of_the_prime)
-        
-        i=1
-        number=1
-        while i < number_of_the_prime
-            number+=2
-            i+=1 if is_prime?(number)
-            puts number
-        end
-        number
+    i = 3
+    while i * i <= number
+      return false if (number % i).zero?
+
+      i += 2
     end
-end
+    true
+  end
+
+  def self.brute_force(number_of_the_prime)
+    raise ArgumentError if number_of_the_prime < 1
+    return 2 if number_of_the_prime == 1
+
+    i = 2
+    number = 3
+    while i < number_of_the_prime
+      number += 2
+      i += 1 if prime?(number)
+    end
+    number
+  end
+  end
--- PE007/prime_finder_spec.rb
+++ PE007/prime_finder_spec.rb
@@ -3,15 +3,23 @@ require_relative 'prime_finder'
 
 describe PrimeFinder do
   it 'finds that 3 is prime number' do
-    expect(PrimeFinder.is_prime?(3)).to eq(true)
+    expect(PrimeFinder.prime?(3)).to eq(true)
   end
 
   it 'finds that 4 is not prime number' do
-    expect(PrimeFinder.is_prime?(4)).to eq(false)
+    expect(PrimeFinder.prime?(4)).to eq(false)
   end
 
   it 'finds that 13 is prime number' do
-    expect(PrimeFinder.is_prime?(4)).to eq(false)
+    expect(PrimeFinder.prime?(4)).to eq(false)
+  end
+
+  it 'finds 1st prime' do
+    expect(PrimeFinder.brute_force(1)).to eq(2)
+  end
+
+  it 'finds 2nd prime' do
+    expect(PrimeFinder.brute_force(2)).to eq(3)
   end
 
   it 'finds 6th prime' do
